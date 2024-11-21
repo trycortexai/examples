4e21ef9a8de7b0f75c1a3183dff51c9b8a7b6c64
arshad <arshadpyaseen@gmail.com>
2024-11-21 21:53:35 +0300

    Make ts happy

--- components/examples/fields-extraction.tsx
+++ components/examples/fields-extraction.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { Demo, DemoResult, FileUploadResult } from "../demo";
+import Demo, { DemoResult, FileUploadResult } from "../demo";
 import { toast } from "sonner";
 
 const FieldsExtraction = () => {
